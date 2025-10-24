2522796ffa2d580383eff2f2e60ba9face18bf68
324a2119 <st324a2119k.matsuda@urayama.ac.jp>
2025-10-24 11:04:06 +0900

    UserDao.java

---
+++ UserDao.java
@@ -0,0 +1,51 @@
+package com.example.cocosuku.dao;
+
+import com.example.cocosuku.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
+@Repository
+public class UserDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public User findByEmail(String email) {
+        String sql = "SELECT * FROM usertb WHERE email = ?";
+        List<User> users = jdbcTemplate.query(sql, (ResultSet rs, int rowNum) -> {
+            User u = new User();
+            u.setUserid(rs.getInt("userid"));
+            u.setEmail(rs.getString("email"));
+            u.setPassword(rs.getString("password"));
+            u.setName(rs.getString("name"));
+            u.setProfile_image(rs.getString("profile_image"));
+            return u;
+        }, email);
+        return users.isEmpty() ? null : users.get(0);
+    }
+
+    public int createUser(User user) {
+        String sql = "INSERT INTO usertb (name, email, password, profile_image) VALUES (?, ?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword()); // 既にハッシュ済み
+            ps.setString(4, user.getProfile_image());
+            return ps;
+        }, keyHolder);
+
+        Number key = keyHolder.getKey();
+        return key != null ? key.intValue() : -1;
+    }
+}
+
