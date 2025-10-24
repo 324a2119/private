f3276b4a2c8d40329e5c0366c164529a0394b3cf
324a2119 <st324a2119k.matsuda@urayama.ac.jp>
2025-10-24 11:23:36 +0900

    LoginController.java

---
+++ LoginController.java
@@ -0,0 +1,28 @@
+package com.example.cocosuku.controller;
+
+import com.example.cocosuku.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public String login(
+            @RequestParam String email,
+            @RequestParam String password,
+            HttpSession session) {
+
+        boolean success = userService.login(email, password);
+        if (success) {
+            session.setAttribute("email", email);
+            return "OK";
+        } else {
+            return "NG";
+        }
+    }
+}
