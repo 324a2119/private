4a622294b0fdced74304ea7c3466adb6a31e2adb
324a2119 <st324a2119k.matsuda@urayama.ac.jp>
2025-11-02 09:24:19 +0900

    Refactor User class with constructor and updated field names

--- model/User.java
+++ model/User.java
@@ -2,25 +2,58 @@ package com.example.cocosuku.model;
 
 public class User {
     private int userid;
+    private String name;
     private String email;
     private String password;
-    private String name;
-    private String profile_image; 
+    private String profileImage;
 
-    // --- getter & setter ---
-    public int getUserid() { return userid; }
-    public void setUserid(int userid) { this.userid = userid; }
+    public User() {}
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public User(int userid, String name, String email, String password, String profileImage) {
+        this.userid = userid;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.profileImage = profileImage;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public int getUserid() {
+        return userid;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
 
-    // 追加
-    public String getProfile_image() { return profile_image; }
-    public void setProfile_image(String profile_image) { this.profile_image = profile_image; }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
 }
